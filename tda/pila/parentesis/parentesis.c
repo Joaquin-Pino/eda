fb062f01197e423334d8ee535a940a4ef812ae29
joaquin <joakopino7@gmail.com>
2025-04-20 23:38:37 -0400

    se anade ejercicio de parentesis andidados

---
+++ tda/pila/parentesis/parentesis.c
@@ -0,0 +1,66 @@
+// programa que determina si una epxresion tiene parentesis balanceados
+// es decir, que cada parenteisis este bien terminado y anidados.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pila.h"
+#include <string.h>
+
+bool estaBalanceado(char expresion[], int len_exp);
+
+int main(){
+    char *expresion = "[(])";
+    printf("%s\n", expresion);
+     
+    int len_exp = strlen(expresion);
+    printf("largo de exp %d\n", len_exp);
+
+    if (estaBalanceado(expresion, len_exp)){
+        printf("los parentesis estan balanceados\n");
+    } else {
+        printf("los parentesis no estan balanceados--\n");
+    }  
+}
+
+bool estaBalanceado(char expresion[], int len_exp){
+    Pila *parentesis = crearPila();
+    
+    for (int i = 0; i < len_exp; i++){ 
+        switch (expresion[i]){
+            case '[':
+                push(parentesis, expresion[i]);
+                break;
+            case '{':
+                push(parentesis, expresion[i]);
+                break;
+            case '(':
+                push(parentesis, expresion[i]);
+                break;
+
+            case ']':
+                if (tope(parentesis) == '['){
+                    pop(parentesis);
+                    break;  
+
+                } else return false;  
+            case '}':
+                if (tope(parentesis) == '{' ){
+                    pop(parentesis);
+                    break;
+                
+                } else return false; 
+            case ')':
+                if (tope(parentesis) == '('){
+                    pop(parentesis);
+                    break;
+
+                } else return false;
+        }  
+    }
+
+    if (estaVacia(parentesis)){
+        return true;
+    }
+    
+    return false;
+}
---
+++ tda/pila/parentesis/pila.h
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 50
+
+typedef struct pila{
+    char datos[MAX];
+    int top;
+}Pila;
+
+bool estaLLena(Pila *pila){
+    if (pila->top + 1== MAX){
+        return true;
+    }
+    return false;
+}
+
+bool estaVacia(Pila *pila){
+    if (pila->top == -1){
+        return true;
+    }
+    return false;
+}
+
+Pila *crearPila(){
+    Pila *mi_pila = (Pila *) malloc(sizeof(Pila));
+    mi_pila->top = -1;
+
+    return mi_pila;
+}
+
+void push(Pila *pila, char valor){
+    if (estaLLena(pila)){      
+        printf("no se ingreso valor, pila llena\n");
+        return;
+    }
+    pila->top++;
+    pila->datos[pila->top] = valor;
+}
+
+char pop(Pila *pila){
+    if (!estaVacia(pila)){
+        char dato = pila->datos[pila->top];
+        pila->top--;
+
+        return dato;
+    }   
+}
+
+char tope(Pila *pila){
+    if (!estaVacia(pila)){
+        return pila->datos[pila->top] ;
+    }
+}
+
+void printPila(Pila *pila){
+    
+    for (int i = 0; i <= pila->top; i++){
+        printf("%c ", pila->datos[i]);
+    }
+    printf("\n");
+}
